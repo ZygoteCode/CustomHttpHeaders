0928b6349bd8875a810f2eaec0154c8d3d853bd4
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-09-10 16:25:40 +0200

    First commit

---
+++ CustomHeader.cs
@@ -0,0 +1,39 @@
+﻿namespace CustomHttpHeaders
+{
+    public class CustomHeader
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public CustomHeader(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetValue()
+        {
+            return Value;
+        }
+
+        public void SetValue(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Value}";
+        }
+    }
+}
